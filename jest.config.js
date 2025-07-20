4fb26d86b0440b81a60a09e60f8f4179b00398fe
Mehul Gupta <mehulgup1001@gmail.com>
2025-07-20 15:08:47 +0800

    fix: use Expo default Jest environment, remove deprecated matchers and jsdom for compatibility

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   preset: 'jest-expo',
-  testEnvironment: 'jsdom',
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|expo|@expo|@react-native-community|@react-navigation|@reactvision/react-viro)',
   ],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }; 
